9ecc719072e2f82925a7d3fb33b71f1399d77963
alexistb2904 <minecrafttablette62@gmail.com>
2024-11-20 09:24:19 +0100

    first commit

---
+++ Code.js
@@ -0,0 +1,67 @@
+// Génération de l'interface ! NE PAS TOUCHER !
+function doGet() {
+	return HtmlService.createTemplateFromFile('index')
+		.evaluate()
+		.setFaviconUrl('https://www.academieduclimat.paris/app/themes/academie-du-climat/src/img/favicons/favicon-32x32.png')
+		.setTitle('Calendrier Académie du Climat')
+		.addMetaTag('viewport', 'width=device-width, initial-scale=1');
+}
+
+function include(filename) {
+	return HtmlService.createHtmlOutputFromFile(filename).getContent();
+}
+
+function getDataFromSpreadsheet(range, sheetChosen = 'base', filterEmpty = true) {
+	try {
+		let sheetIDToUse = sheetID;
+		var values = Sheets.Spreadsheets.Values.batchGet(sheetIDToUse, { ranges: [range] });
+		if (values.valueRanges[0].values) {
+			return filterEmpty ? values.valueRanges[0].values.filter((row) => row != '') : values.valueRanges[0].values;
+		} else {
+			return [];
+		}
+	} catch (error) {
+		Logger.log('Error: ' + error);
+		console.error(error.message);
+		return error.message;
+	}
+}
+
+function convertAbregedStringToDate(dateString, lineInTable = null) {
+	const monthsMap = {
+		'janv.': 0,
+		'févr.': 1,
+		mars: 2,
+		'avr.': 3,
+		mai: 4,
+		juin: 5,
+		'juil.': 6,
+		août: 7,
+		'sept.': 8,
+		'oct.': 9,
+		'nov.': 10,
+		'déc.': 11,
+		janvier: 0,
+		février: 1,
+		avril: 3,
+		juillet: 6,
+		septembre: 8,
+		octobre: 9,
+		novembre: 10,
+		décembre: 11,
+	};
+
+	try {
+		let dateSplit = dateString.split(' ');
+		let day = parseInt(dateSplit[1]);
+		let month = monthsMap[dateSplit[2]];
+		let year = parseInt(dateSplit[3]) + 2000;
+		// Création de la date en spécifiant l'heure à midi pour éviter les problèmes de fuseau horaire
+		return new Date(Date.UTC(year, month, day, 12, 0, 0, 0));
+	} catch (error) {
+		Logger.log('Erreur à la ligne ' + lineInTable);
+		Logger.log(dateString);
+		Logger.log('Erreur : ' + error);
+		return new Error('Erreur' + error);
+	}
+}
---
+++ variables.js
@@ -0,0 +1,103 @@
+const sheetID = '1ttflCuSrJfONsgaH9TDvGqAYyiBT2kq7GikNi918vJc';
+
+const colonneMin = 'A';
+const colonneMax = 'AE';
+
+// Emplacement Programmation
+const COL_CONFIRMATION = 0;
+const COL_DATE = 1;
+const COL_HORAIRE_DEB = 2;
+const COL_HORAIRE_FIN = 3;
+const COL_LIEU = 4;
+const COL_TITRE = 5;
+const COL_TYPE = 6;
+const COL_AGENDA = 7;
+const COL_PUBLIC_CIBLE = 8;
+const COL_TYPE_STRUCTURE = 9;
+const COL_NOM_STRUCTURE = 10;
+const COL_RESUME = 11;
+const COL_THEMATIQUE = 12;
+const COL_THEMATIQUE_2 = 13;
+const COL_EFFECTIFS = 14;
+const COL_LOGISTIQUE = 15;
+const COL_STATIQUE = 16;
+const COL_CAPTATION = 17;
+const COL_PRESENTATION_ADC = 18;
+const CONVENTION_MISE_DISPO = 19;
+const COL_ETAT_CONVENTION = 20;
+const COL_REF_ADC = 21;
+const COL_INFO_CONTACT_EXT = 22;
+const COL_MAIL_EXT = 23;
+const COL_TEL_EXT = 24;
+const COL_GOOGLE_AGENDA = 25;
+const COL_AGENDA_ID = 26;
+const COL_ID = 27;
+const COL_LASTEDIT = 28;
+const COL_LASTEDITOR = 29;
+const COL_FOLDERURL = 30;
+
+const COL_ID_LETTER = 'AB';
+
+// Emplacement Médiation
+const MED_COL_CONFIRMATION = 0;
+const MED_COL_DATE = 1;
+const MED_COL_HORAIRE_DEB = 2;
+const MED_COL_HORAIRE_FIN = 3;
+const MED_COL_LIEU = 4;
+const MED_COL_TYPE_MODULE = 5;
+const MED_COL_DETAIL = 6;
+const MED_COL_TYPE_PUBLIC = 7;
+const MED_COL_EFFECTIFS = 8;
+const MED_COL_LOGISTIQUE = 9;
+const MED_COL_MEDIATION = 10;
+const MED_NOM_ECOLE = 11;
+const MED_CODE_POSTAL = 12;
+const MED_NIVEAU_ETAB = 13;
+const MED_NIVEAU_GROUPE = 14;
+const MED_EDU_PRIO = 15;
+const MED_EDU_PRECISION = 16;
+const MED_COL_REF_ADC = 17;
+const MED_COL_INFO_CONTACT_EXT = 18;
+const MED_COL_MAIL_EXT = 19;
+const MED_COL_TEL_EXT = 20;
+const MED_COL_AJOUTE = 21;
+const MED_COL_ID = 22;
+const MED_COL_LAST_EDIT = 24;
+const MED_COL_LAST_EDITOR = 25;
+const MED_COL_FOLDERURL = 26;
+
+const MED_COL_ID_LETTER = 'W';
+
+let materialInAcademie = [
+	{ name: 'Armoire Électrique Portative', amount: 1 },
+	{ name: 'Bancs', amount: 2 },
+	{ name: 'Banquette', amount: 0 },
+	{ name: 'Bûche en bois', amount: 4 },
+	{ name: 'Câble son', amount: 2 },
+	{ name: 'Chaises', amount: 657 },
+	{ name: 'Chaises Hautes', amount: 7 },
+	{ name: 'Chevalet', amount: 0 },
+	{ name: 'Cube bloc blanc', amount: 8 },
+	{ name: 'Dibond ADC', amount: 2 },
+	{ name: 'Ecran', amount: 4 },
+	{ name: 'Enrouleur', amount: 10 },
+	{ name: 'Etablis', amount: 3 },
+	{ name: 'Fauteuil', amount: 13 },
+	{ name: 'Grilles Caddies', amount: 12 },
+	{ name: 'Mange debout', amount: 7 },
+	{ name: 'Micro', amount: 10 },
+	{ name: 'Multi Prise', amount: 10 },
+	{ name: 'Paperboard', amount: 0 },
+	{ name: 'Piano', amount: 2 },
+	{ name: 'Portant', amount: 4 },
+	{ name: 'Potences', amount: 16 },
+	{ name: 'Praticable', amount: 9 },
+	{ name: 'Poufs', amount: 2 },
+	{ name: 'Sono Mobile', amount: 2 },
+	{ name: 'Tableau blanc', amount: 1 },
+	{ name: 'Tabouret', amount: 17 },
+	{ name: 'Tables', amount: 119 },
+	{ name: 'Tables Basse', amount: 2 },
+	{ name: 'TV', amount: 8 },
+	{ name: 'Vidéo Projecteur', amount: 6 },
+];
